f3811f8f2b86b84fb2f21358610f2e97e0659c81
Victormbaabu <victor.mbaabu@strathmore.edu>
2021-11-28 17:20:45 +0300

    Added delivery control flow

--- js/main.js
+++ js/main.js
@@ -45,3 +45,64 @@ $(document).ready(function() {
           inputForUserOrder.crust() +
           inputForUserOrder.toppings()) *
         inputForUserOrder.quantity();
+        //control user input
+      if (totalPrice > 0) {
+        //total user order price
+        alert("Total order is = ksh" + totalPrice);
+  
+        //prompt for delivery
+        var delivery = confirm(
+          "We can deliver your order to your location for an additional 100. Would you want that?"
+        );
+  
+        //delivery control flow
+        if (delivery === true) {
+          //infinite loop to control correct location entry
+          for (;;) {
+            var location = prompt(
+              "Please enter the location where you want your order delivered: "
+            );
+  
+            //checking validity of user input
+            if (location !== "") {
+              alert(
+                "Your order will be delivered here: " +
+                  location +
+                  ". Thank you for choosing us! Order again soon"
+              );
+  
+              break;
+            } else {
+              alert(
+                "Please enter a valid location to have your order delivered!"
+              );
+            }
+          }
+  
+          //final prompt
+          alert(
+            "In case you missed it... Your total order is = ksh" +
+              totalPrice +
+              " + ksh100 delivery fee."
+          );
+        } else {
+          alert(
+            "Thank you for choosing us! Your order has been processed, pass by our restaurant to pick it."
+          );
+  
+          //final prompt
+          alert(
+            "In case you missed it... Your total order is = ksh" + totalPrice
+          );
+        }
+  
+        //reset form
+        $("#get-pizza-order").reset();
+      } else {
+        alert("Please fill in all the form inputs to make an order!");
+      }
+  
+      event.preventDefault();
+    });
+  });
+  
