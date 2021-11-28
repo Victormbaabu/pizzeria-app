8802e3def92ac5480d36bc941c4156b097881c93
Victormbaabu <victor.mbaabu@strathmore.edu>
2021-11-28 13:57:22 +0300

    Adding form submission function and constructor for ordering pizza data

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// backend logic section
+$(document).ready(function() {
+    // form submission function
+    $("#get-pizza-order").submit(function(event) {
+        
+      // a constructor to define data needed in ordering pizza
+      function UserOrder(size, crust, toppings, quantity) {
+        this.size = size;
+        this.crust = crust;
+        this.toppings = toppings;
+        this.quantity = quantity;
+      }
+  
+      // fetching user data - price
+      var pizzaSizePrice = function getSize() {
+        var pizzaSize = document.getElementById("pizza-size").value;
+        return parseInt(pizzaSize);
+      };
+  
+      var pizzaCrustPrice = function getCrustType() {
+        var pizzaCrust = document.getElementById("crust-type").value;
+        return parseInt(pizzaCrust);
+      };
+  
+      var pizzaToppingsPrice = function getToppings() {
+        var pizzaToppings = document.getElementById("pizza-toppings").value;
+        return parseInt(pizzaToppings);
+      };
+  
+      var pizzaQuantityPrice = function getQuantity() {
+        var pizzaQuantity = document.getElementById("pizza-quantity").value;
+        return parseInt(pizzaQuantity);
+      };
